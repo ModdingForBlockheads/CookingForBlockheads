c962e4c8fd4a0fcd40937c271bac5a7f3c728872
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2024-10-31 13:00:53 +0100

    wip: Fix compile error on Fabric

--- common/src/main/java/net/blay09/mods/cookingforblockheads/menu/CuttingBoardMenu.java
+++ common/src/main/java/net/blay09/mods/cookingforblockheads/menu/CuttingBoardMenu.java
@@ -10,8 +10,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.*;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class CuttingBoardMenu extends AbstractCraftingMenu {
