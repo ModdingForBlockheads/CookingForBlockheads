5a94fa6aeca15a641299faeacca1898882b5b32a
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2025-01-13 20:25:34 +0100

    feat: Update to Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/cookingforblockheads/ForgeCookingForBlockheads.java
+++ forge/src/main/java/net/blay09/mods/cookingforblockheads/ForgeCookingForBlockheads.java
@@ -1,8 +1,8 @@
 package net.blay09.mods.cookingforblockheads;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.balm.forge.provider.ForgeBalmProviders;
 import net.blay09.mods.cookingforblockheads.api.KitchenItemProvider;
 import net.blay09.mods.cookingforblockheads.api.KitchenItemProcessor;
@@ -10,14 +10,13 @@ import net.blay09.mods.cookingforblockheads.api.event.OvenItemSmeltedEvent;
 import net.blay09.mods.cookingforblockheads.client.CookingForBlockheadsClient;
 import net.blay09.mods.cookingforblockheads.compat.Compat;
 import net.blay09.mods.cookingforblockheads.compat.TheOneProbeAddon;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(CookingForBlockheads.MOD_ID)
 public class ForgeCookingForBlockheads {
@@ -28,17 +27,20 @@ public class ForgeCookingForBlockheads {
     public static Capability<KitchenItemProcessor> KITCHEN_ITEM_PROCESSOR_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
     });
 
-    public ForgeCookingForBlockheads() {
+    public ForgeCookingForBlockheads(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
         Balm.getEvents().onEvent(OvenItemSmeltedEvent.class, orig -> {
             PlayerEvent.ItemSmeltedEvent event = new PlayerEvent.ItemSmeltedEvent(orig.getPlayer(), orig.getResultItem());
             MinecraftForge.EVENT_BUS.post(event);
         });
 
-        Balm.initialize(CookingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, CookingForBlockheads::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(CookingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, CookingForBlockheadsClient::initialize));
+        Balm.initialize(CookingForBlockheads.MOD_ID, loadContext, CookingForBlockheads::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(CookingForBlockheads.MOD_ID, loadContext, CookingForBlockheadsClient::initialize);
+        }
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerCapabilities);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        context.getModEventBus().addListener(this::registerCapabilities);
+        context.getModEventBus().addListener(this::enqueueIMC);
 
         ForgeBalmProviders providers = (ForgeBalmProviders) Balm.getProviders();
         providers.register(KitchenItemProvider.class, new CapabilityToken<>() {
